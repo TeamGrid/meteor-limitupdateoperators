a59b7a30ebef734a9f3d6638d3e86600365f5905
Max Nowack <max@unsou.de>
2018-02-14 17:43:45 +0100

    coffeescript@2.0.0

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
 Package.describe({
   name: 'teamgrid:limit-update-operators',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Easy limiting of mongo update operators (deny rule)',
   git: 'https://github.com/TeamGrid/meteor-limitupdateoperators.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('coffeescript');
+  api.versionsFrom('1.6.1');
+  api.use('coffeescript@2.0.0');
   api.use('underscore');
   api.use('check');
   api.use('lai:collection-extensions@0.2.1_1');
